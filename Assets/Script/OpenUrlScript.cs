196cd401db199bccb8a34d73ec58f97d8f42cf35
kayuminbou <koyama2199.ssk@gmail.com>
2018-09-19 22:28:05 +0900

    add button

---
+++ Assets/Script/OpenUrlScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrlScript : MonoBehaviour {
+    
+    public void OpenUrl()
+    {
+        Application.OpenURL("https://github.com/kayuminbou");
+    }
+}
